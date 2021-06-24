baa45c3841b9cc082d4aa099ad4a21a37471b041
shamolmojumder <shamolmojumder14@gmail.com>
2021-06-25 02:13:05 +0600

    project file added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome} from "@fortawesome/free-solid-svg-icons";
+function App() {
+  return (
+    <div>
+      <h1>Sports Mania</h1>
+      <FontAwesomeIcon icon={faHome} />
+
+    </div>
+  );
+}
+
+export default App;
