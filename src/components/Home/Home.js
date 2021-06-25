6b460ca531c3fb5af1cbb583db08de9cdcf9c518
shamolmojumder <shamolmojumder14@gmail.com>
2021-06-25 12:05:30 +0600

    league.map showings error

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { Container } from '@material-ui/core';
+import { useEffect } from 'react';
+import League from '../League/League';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,33 +18,34 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
+
 const Home = () => {
     const classes = useStyles();
+    const [leagues,setLeagues] = useState({});
+    console.log(leagues);
+    useEffect(()=>{
+      fetch(`https://www.thesportsdb.com/api/v1/json/1/all_leagues.php`)
+      .then(res=>res.json())
+      .then(data=>setLeagues(data.leagues))
+    },[])
     return (
-        <Container maxWidth="sm">
+        <Container fixed >
         <div className={classes.root}>
-        <Grid container spacing={2}>
-          <Grid item sm={12}>
-            <img src="https://cutt.ly/Jn6gUoq" alt="" />
+        <Grid container className="banner">
+          <Grid item lg={12}>
+            <img style={{width:"100%"}} src="https://cutt.ly/Jn6gUoq" alt="" />
           </Grid>
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>xs=6</Paper>
+          
+          <Grid item lg={12}>
+            <Paper classame={classes.paper}>  
+              <h1 style={{textAlign:"center"}}> Total leagues {leagues.length}</h1>
+            </Paper>
           </Grid>
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>xs=6</Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=3</Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=3</Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=3</Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.paper}>xs=3</Paper>
-          </Grid>
+
+          {
+            leagues.map(league=> <League league={league}> <h1>hello</h1> </League>  )
+          }
         </Grid>
       </div>
       </Container>
