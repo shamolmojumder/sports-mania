07ae996df241fc42feaa63dd833e3a19c145bf5a
shamolmojumder <shamolmojumder14@gmail.com>
2021-07-04 12:29:29 +0600

    7/4/21

--- src/components/TeamDetail/TeamDetail.js
+++ src/components/TeamDetail/TeamDetail.js
@@ -4,11 +4,12 @@ import { useParams } from 'react-router-dom';
 
 const TeamDetail = () => {
     const{idTeam}= useParams();
-    const [team,setTeam]=useState([]);
+    const [team,setTeam]=useState({});
+    const {strDescriptionEN,strFacebook,strGender,strInstagram,strLeague,strLeague2,strLeague3,strStadiumThumb,strSport,intFormedYear}=team;
     useEffect(()=>{
         fetch(`https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idTeam}`)
         .then(res=>res.json())
-        .then(data=>setTeam(data.teams))
+        .then(data=>setTeam(data.teams[0]))
     },[idTeam])
     console.log("team detail",team);
     return (
@@ -17,7 +18,20 @@ const TeamDetail = () => {
                 <img src='https://i.ibb.co/Hx22hkG/Capture.png' alt="" />
             </div>
             <h1>Detail {idTeam} </h1>
-            <p> {team[0].strDescriptionEN} </p>
+            <div className="premier-league-section">
+             <div style={{backgroundColor:'#183386'}} className="premier-league-left">
+             <h4>Premier league  </h4>
+                <h3>Founded: {intFormedYear} </h3>
+                <h3>Sport Type: {strSport} </h3>
+                <h3>Gender:{strGender} </h3>
+             </div>
+             <div className="premier-league-right">
+
+             </div>
+            </div>
+            <p> {strDescriptionEN} </p>
+            {/* <p> {team[0].strDescriptionEN === undefined? "found" :"no info found"} </p> */}
+            {/* {strAlternate.length ? "{strAlternate}": "no info found" } */}
         </div>
     );
 };
